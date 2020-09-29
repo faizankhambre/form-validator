13a354a52afa1f134908d71f75450401106aeb98
faizankhambre <72085478+faizankhambre@users.noreply.github.com>
2020-09-29 22:49:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+const form = document.getElementById('form'); 
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+//function show input error msg
+function showError(input, message)
+{
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+//show success ouline
+function showSuccess(input)
+{
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+    
+}
+
+//check email is valid
+function checkEmail(input)
+{
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
+    if(re.test(input.value.trim()))
+    {
+       showSuccess(input);
+    }
+    else
+    {
+       showError(input, 'Email is not valid');
+    }
+}
+
+//check required field
+function checkRequired(inputArr)
+{
+    inputArr.forEach(function(input) {
+        if(input.value.trim() === '')
+        {
+            showError(input, `${input.id} is required`);
+        }
+        else
+        {
+            showSuccess(input);
+        }
+    });
+}
+
+//check input length
+function checkLength(input, min, max)
+{
+    if(input.value.length < min)
+    {
+       showError(input, `${input.id} must be atlest ${min} Characters`);
+    }
+    
+    else if(input.value.length > max)
+    {
+        showError(input, `${input.id} must be less than ${max} Characters`);    
+    }
+    
+    else
+    {
+        showSuccess(input);
+    }
+}
+
+//check passwords match
+function checkPasswordsMatch(input1, input2)
+{
+    if(input1.value !== input2.value)
+    {
+       showError(input2, 'Password do not Match');
+    }
+}
+
+//Event Listners
+form.addEventListener('submit', function(e) {
+    
+    e.preventDefault();
+    
+    checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 15);//min is 3 and max is 15 
+    checkLength(password, 6, 25);//min is 6 and max is 25
+    checkEmail(email);
+    checkPasswordsMatch(password, password2);
+    
+});
